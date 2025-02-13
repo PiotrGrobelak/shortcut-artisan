d0264663c7065a861c8c5f9bfc7d5fcae3db5539
PiotrGrobelak <grobelak.piotr@gmail.com>
2025-02-13 23:14:40 +0100

    feat: add colors for logs levels

--- src-tauri/src/analytics/plugins.rs
+++ src-tauri/src/analytics/plugins.rs
@@ -3,6 +3,24 @@ use tauri::Runtime;
 pub fn setup_logging_plugin<R: Runtime>() -> TauriPlugin<R> {
     tauri_plugin_log::Builder::new()
         .clear_targets()
+        .format(|out, message, record| {
+            let color = match record.level() {
+                log::Level::Error => "\x1b[31m",
+                log::Level::Warn => "\x1b[33m",
+                log::Level::Info => "\x1b[94m",
+                log::Level::Debug => "\x1b[36m",
+                log::Level::Trace => "\x1b[35m",
+            };
+            let reset = "\x1b[0m";
+            out.finish(format_args!(
+                "{}[{}][{}] {}{}",
+                color,
+                record.level(),
+                record.target(),
+                message,
+                reset
+            ))
+        })
         .target(tauri_plugin_log::Target::new(
             tauri_plugin_log::TargetKind::Stdout,
         ))
